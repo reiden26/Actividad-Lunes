9c53a65e4a59d97dcf54357319434d027e29e50c
reiden26 <rheiden77@gmail.com>
2021-10-05 20:54:07 -0500

    Agregamos el taller a github

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactenosComponent } from './paginas/contactenos/contactenos.component';
+import { EntradasComponent } from './paginas/entradas/entradas.component';
+import { HorariosComponent } from './paginas/horarios/horarios.component';
+import { PrincipalComponent } from './paginas/principal/principal.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
+const routes:Routes=[
+{path:'principal',component:PrincipalComponent},
+{path:'registro', component:RegistroComponent},
+{path:'contactenos',component:ContactenosComponent},
+{path:'horarios',component:HorariosComponent},
+{path:'entradas', component:EntradasComponent},
+{path:'',redirectTo:'/principal',pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
